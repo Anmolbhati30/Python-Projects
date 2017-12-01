cce83c76c89802ddbce21f698c8c834248dddba2
Anmolbhati30 <33755829+Anmolbhati30@users.noreply.github.com>
2017-12-01 12:17:58 +0000

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,10 +1,14 @@
 def prime():
     n = int(input("Num: "))
-
+    if n == 0:
+        print("Not Prime")
+    elif n == 1 or n == 2:
+        print("Prime")
+    
     if all(n%x != 0 for x in range(2,n)):
-        print("True")
+        print("Prime")
     else:
-        print("False")
+        print("Not Prime")
     prime()
 
 
